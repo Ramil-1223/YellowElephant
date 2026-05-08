e6d96262c8547ce0ef6892cebce5b934c7796266
Ramil <kajrlievrami@gmail.com>
2026-05-09 01:39:41 +0300

    test: add parsing test for multi-line strings using pytest

--- client/data/http_paths.py
+++
@@ -1,4 +0,0 @@
-http_create = "http://192.168.119.61:9999/create_base"
-http_drop = "http://192.168.119.61:9999/drop_base"
-http_backup = "http://192.168.119.61:9999/backup_base"
-http_list = "http://192.168.119.61:9999/list_base"
---
+++ server/test_py/test_list.py
@@ -0,0 +1,55 @@
+import pytest, re
+
+@pytest.fixture
+def db_list():
+    base_list = """
+infobase : 4b2f4b2f-4b2f-4b2f-4b2f-4b2f4b2f4b2f
+name     : base_name1
+descr    : \"Description\"
+
+infobase : d1a9d1a9-d1a9-d1a9-d1a9-d1a9d1a9d1a9
+name     : base_name2
+descr    : \"Description\"
+
+infobase : e5c8e5c8-e5c8-e5c8-e5c8-e5c8e5c8e5c8
+name     : base_name3
+descr    : \"Description\"
+"""
+    return base_list
+
+@pytest.fixture
+def parsed_infobases(db_list):
+
+    pattern = (
+        r"infobase\s*:\s*(?P<infobase>[a-f0-9-]{36})\s*\n"
+        r"name\s*:\s*(?P<name>[^\s\n]+)\s*\n"
+        r"descr\s*:\s*.*"
+    )
+
+    matches = re.finditer(pattern, db_list, re.MULTILINE)
+    infobases = [match.groupdict() for match in matches]
+    return infobases
+
+@pytest.fixture
+def dict_parsed_infobases(parsed_infobases):
+    list_base = [item['name'] for item in parsed_infobases]
+    return list_base
+
+@pytest.fixture
+def list_parsed_infobases(dict_parsed_infobases):
+    str_base = '\n'.join(dict_parsed_infobases)
+    return str_base
+
+
+def test_output_dict(parsed_infobases):
+    assert parsed_infobases == [
+                {'infobase': '4b2f4b2f-4b2f-4b2f-4b2f-4b2f4b2f4b2f', 'name': 'base_name1'},
+                {'infobase': 'd1a9d1a9-d1a9-d1a9-d1a9-d1a9d1a9d1a9', 'name': 'base_name2'},
+                {'infobase': 'e5c8e5c8-e5c8-e5c8-e5c8-e5c8e5c8e5c8', 'name': 'base_name3'}
+                ]
+    
+def test_output_list(dict_parsed_infobases):
+    assert dict_parsed_infobases == ['base_name1', 'base_name2', 'base_name3']
+
+def test_output_string(list_parsed_infobases):
+    assert list_parsed_infobases == "base_name1\nbase_name2\nbase_name3"
