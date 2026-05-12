b915fcf9b6203b8eded6d2f2c5b66f660c56cf62
Ramil <kajrlievrami@gmail.com>
2026-05-13 00:05:55 +0300

    test: changed pytest fixtures scope to module

--- server/test_py/test_multiline.py
+++ server/test_py/test_multiline.py
@@ -1,6 +1,6 @@
 import pytest, re
 
-@pytest.fixture
+@pytest.fixture(scope = 'module')
 def db_list():
     base_list = """
 infobase : 4b2f4b2f-4b2f-4b2f-4b2f-4b2f4b2f4b2f
@@ -17,7 +17,8 @@ descr    : \"Description\"
 """
     return base_list
 
-@pytest.fixture
+
+@pytest.fixture(scope = 'module')
 def parsed_infobases(db_list):
 
     pattern = (
@@ -30,15 +31,15 @@ def parsed_infobases(db_list):
     infobases = [match.groupdict() for match in matches]
     return infobases
 
-@pytest.fixture
-def dict_parsed_infobases(parsed_infobases):
-    list_base = [item['name'] for item in parsed_infobases]
-    return list_base
 
-@pytest.fixture
+@pytest.fixture(scope = 'module')
+def dict_parsed_infobases(parsed_infobases):
+    return [item['name'] for item in parsed_infobases]
+
+
+@pytest.fixture(scope = 'module')
 def list_parsed_infobases(dict_parsed_infobases):
-    str_base = '\n'.join(dict_parsed_infobases)
-    return str_base
+    return '\n'.join(dict_parsed_infobases)
 
 
 def test_output_dict(parsed_infobases):
@@ -47,9 +48,11 @@ def test_output_dict(parsed_infobases):
                 {'infobase': 'd1a9d1a9-d1a9-d1a9-d1a9-d1a9d1a9d1a9', 'name': 'base_name2'},
                 {'infobase': 'e5c8e5c8-e5c8-e5c8-e5c8-e5c8e5c8e5c8', 'name': 'base_name3'}
                 ]
-    
+
+
 def test_output_list(dict_parsed_infobases):
     assert dict_parsed_infobases == ['base_name1', 'base_name2', 'base_name3']
+
 
 def test_output_string(list_parsed_infobases):
     assert list_parsed_infobases == "base_name1\nbase_name2\nbase_name3"
